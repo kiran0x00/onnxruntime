75a146fd86c9b6149a2864834ba5acc185648030
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-06-12 13:23:16 -0700

    TEMPORARY: Add ORTTrainerOptions class and 'optim' and 'amp' submodules

---
+++ orttraining/orttraining/test/python/orttraining_test_orttrainer_frontend.py
@@ -0,0 +1,60 @@
+import pytest
+import torch
+
+from onnxruntime.capi.training import orttrainer_options as orttrainer_options
+
+
+@pytest.mark.parametrize("test_input", [
+    ({}),
+    ({'batch': {},
+      'device': {},
+      'distributed': {},
+      'mixed_precision': {},
+      'utils': {},
+      '_internal_use': {}})
+])
+def testORTTrainerOptionsDefaultValues(test_input):
+    ''' Test different ways of using default values for incomplete input'''
+
+    expected_values = {
+        'batch': {
+            'gradient_accumulation_steps': 0
+        },
+        'device': {
+            'id': None,
+            'mem_limit': 0
+        },
+        'distributed': {
+            'world_rank': 0,
+            'world_size': 1,
+            'local_rank': 0,
+            'allreduce_post_accumulation': False,
+            'enable_partition_optimizer': False,
+            'enable_adasum': False
+        },
+        'lr_scheduler': None,
+        'mixed_precision': {
+            'enabled': False,
+            'loss_scaler': None
+        },
+        'utils': {
+            'grad_norm_clip': False
+        },
+        '_internal_use': {
+            'frozen_weights': [],
+            'enable_internal_postprocess': True,
+            'extra_postprocess': None
+        }
+    }
+
+    actual_values = orttrainer_options.ORTTrainerOptions(test_input)
+    assert actual_values._validated_opts == expected_values
+
+
+def testORTTrainerOptionsInvalidMixedPrecisionEnabledSchema():
+    '''Test an invalid input based on schema validation error message'''
+
+    expected_msg = "Invalid options: {'mixed_precision': [{'enabled': ['must be of boolean type']}]}"
+    with pytest.raises(ValueError) as e:
+        orttrainer_options.ORTTrainerOptions({'mixed_precision': {'enabled': 1}})
+    assert str(e.value) == expected_msg
